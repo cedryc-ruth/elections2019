f7527f98fc22d079b1210c36f1d5140e88932475
S08_ICC <S08_ICC@vbx-schools.brunette.brucity.be>
2019-02-25 20:49:29 +0100

    Définition de la structure des données

---
+++ src/poo/iccbxl/be/MainApplication.java
@@ -0,0 +1,39 @@
+package poo.iccbxl.be;
+
+import java.util.TreeMap;
+
+public class MainApplication {
+
+	public static void main(String[] args) {
+		//Lecture des données
+	
+		//Définition d'un objet Mock
+	int[] dataListeA = { 32000, 2, 0 };
+	int[] dataListeB = { 25000, 2, 0 };
+	int[] dataListeC = { 16000, 1, 0 };
+	int[] dataListeD = { 12500, 1, 0 };
+	int[] dataListeE = { 8000, 0, 0 };
+	int[] dataListeF = { 4500, 0, 1 };
+	int[] dataListeG = { 2500, 0, 1 };
+	
+	TreeMap<String,int[]> listes = new TreeMap<String, int[]>();
+	
+	listes.put("A", dataListeA);
+	listes.put("B", dataListeB);
+	listes.put("C", dataListeC);
+	listes.put("D", dataListeD);
+	listes.put("E", dataListeE);
+	listes.put("F", dataListeF);
+	listes.put("G", dataListeG);
+	
+
+		//Traitement des données (Méthode de calcul)
+		
+		
+		//Affichage du résultat
+		
+		
+
+	}
+
+}
